5f58f6cb4d0dec858408a5b860af3045642cd3db
samcassie <samgcassie@gmail.com>
2020-07-03 22:20:29 +0100

    Started set-up

---
+++ src/components/QuizCategoryGrid.vue
@@ -0,0 +1,25 @@
+<template lang="html">
+
+    <div class="">
+    <v-container>
+        <quiz-category-panel />
+        <quiz-category-panel />
+        <quiz-category-panel />
+    </v-container>
+    </div>
+
+</template>
+
+<script>
+import QuizCategoryPanel from '@/components/QuizCategoryPanel';
+
+export default {
+    name: "QuizGrid",
+    components: {
+        'quiz-category-panel': QuizCategoryPanel
+    }
+}
+</script>
+
+<style lang="css" scoped>
+</style>
