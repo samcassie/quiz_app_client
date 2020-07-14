f1449802a0394a51c79a08a25808d75238124aa3
Robb McQuiston <robb@quiz.com>
2020-07-14 23:21:50 +0100

    Adding files to allow deployment to Gitlab pages

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,6 @@
 module.exports = {
   "transpileDependencies": [
     "vuetify"
-  ]
+  ],
+  publicPath: process.env.NODE_ENV === 'production' ? '/' + process.env.CI_PROJECT_NAME + '/' : '/'  //This code is required to deploy to Gitlab Pages
 }
